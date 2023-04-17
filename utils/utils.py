30890cac32fc842bb58e71f036f7b491994d18fc
Tzlil Ovadia <tzlil.ovadia@mail.huji.ac.il>
2023-04-17 21:06:20 +0300

    added ex2 file & edited utils file

---
+++ utils/utils.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+import numpy as np
+
+MAC_OS_PATH = "../dataset/sequences/05/"
+WINDOWS_OS_PATH = "../dataset/sequences/05\\"
+SEP = "\\" if os.name == 'nt' else "/"
+DATA_PATH = WINDOWS_OS_PATH if os.name == 'nt' else MAC_OS_PATH
+
+def read_images(idx) -> (np.ndarray, np.ndarray):
+    """
+    Read two images (img1 and img2) given an index and assign them to the Matcher object.
+    :param idx: An integer file index for the images to match.
+    :return: None
+    """
+    img_name = '{:06d}.png'.format(idx)
+    _img1 = cv2.imread(DATA_PATH + f'image_0{SEP}' + img_name, 0)
+    _img2 = cv2.imread(DATA_PATH + f'image_1{SEP}' + img_name, 0)
+    return _img1, _img2
