2b5c5348e250c0a60d3427a25628928186d60d5c
Tzlil Ovadia <tzlil.ovadia@mail.huji.ac.il>
2023-04-18 18:54:43 +0300

    section 2.2 plot matches implemented

--- utils/plotters.py
+++ utils/plotters.py
@@ -1,9 +1,9 @@
+import numpy as np
 from matplotlib import pyplot as plt
 
 
 def gen_hist(data, bins, title="", x="X", y="Y"):
     """
-
     :param data:
     :param bins:
     :return:
@@ -14,3 +14,28 @@ def gen_hist(data, bins, title="", x="X", y="Y"):
     plt.ylabel(y)
     plt.xlabel(x)
     plt.show()
+
+
+def draw_matches(img1, kp1, img2, kp2, img1in, img2in, img1out, img2out, title=""):
+    x1_in, y1_in = np.array([kp1[idx].pt for idx in img1in], dtype=np.int).T
+    x2_in, y2_in = np.array([kp2[idx].pt for idx in img2in], dtype=np.int).T
+    x1_out, y1_out = np.array([kp1[idx].pt for idx in img1out], dtype=np.int).T
+    x2_out, y2_out = np.array([kp2[idx].pt for idx in img2out], dtype=np.int).T
+
+    fig = plt.figure()
+    fig.suptitle(title)
+    fig.add_subplot(2, 1, 1)
+
+    fig.add_subplot(2, 1, 1)
+    draw_layer(img1, x1_in, x1_out, y1_in, y1_out)
+
+    fig.add_subplot(2, 1, 2)
+    draw_layer(img2, x2_in, x2_out, y2_in, y2_out)
+
+    fig.show()
+
+
+def draw_layer(img2, x2_in, x2_out, y2_in, y2_out, s=1):
+    plt.imshow(img2, cmap='gray')
+    plt.scatter(x2_in, y2_in, s=s, color='cyan')
+    plt.scatter(x2_out, y2_out, s=s, color='orange')
--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,7 @@ WINDOWS_OS_PATH = "../dataset/sequences/05\\"
 SEP = "\\" if os.name == 'nt' else "/"
 DATA_PATH = WINDOWS_OS_PATH if os.name == 'nt' else MAC_OS_PATH
 
+
 def read_images(idx) -> (np.ndarray, np.ndarray):
     """
     Read two images (img1 and img2) given an index and assign them to the Matcher object.
@@ -17,3 +18,13 @@ def read_images(idx) -> (np.ndarray, np.ndarray):
     _img1 = cv2.imread(DATA_PATH + f'image_0{SEP}' + img_name, 0)
     _img2 = cv2.imread(DATA_PATH + f'image_1{SEP}' + img_name, 0)
     return _img1, _img2
+
+
+def coords_from_kps(matches, kp1,kp2):
+    query_idxs = np.array([match[0].queryIdx for match in matches])
+    train_idxs = np.array([match[0].trainIdx for match in matches])
+    indices_mapping = np.stack((query_idxs,train_idxs))
+    x1, y1 = np.array([kp1[idx].pt for idx in query_idxs]).T
+    x2, y2 = np.array([kp2[idx].pt for idx in train_idxs]).T
+
+    return x1, y1, x2, y2, indices_mapping
