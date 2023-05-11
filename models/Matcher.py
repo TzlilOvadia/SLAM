5e6d76b7bdd2be3e6673d1e01f01c538508b5d51
Tzlil Ovadia <66905917+TzlilOvadia@users.noreply.github.com>
2023-05-11 18:37:39 +0300

    up to 3.2 (#9)

--- models/Matcher.py
+++ models/Matcher.py
@@ -3,13 +3,19 @@ import numpy as np
 from matplotlib import pyplot as plt
 import random
 from utils.utils import read_images
-
+from utils.plotters import draw_matches
 # noinspection PyUnresolvedReferences
+MATCHES = 2
+LEFT = 0
+RIGHT = 1
+CONSECUTIVE = 3
+FRAMES = 4
 
-
+HORIZONTAL_REPRESENTATION = 0
+VERTICAL_REPRESENTATION = 1
 class Matcher:
     """A class for matching features between two images using a specified algorithm and matcher."""
-    def __init__(self, algo, matcher=cv2.BFMatcher(), file_index=0, threshold=.2):
+    def __init__(self, algo=cv2.SIFT_create(), matcher=cv2.BFMatcher(), file_index=0, threshold=.2, display=HORIZONTAL_REPRESENTATION):
         """
         Initialize a Matcher object with an algorithm, a matcher, and a file index.
         :param algo: A cv2 algorithm object (e.g. cv2.SIFT_create(), cv2.AKAZE_create(), cv2.ORB_create()).
@@ -17,6 +23,10 @@ class Matcher:
         :param file_index: An integer file index for the images to match.
         :param threshold: A float in the range on [0,1], used in order to filter descriptors by their significance.
         """
+        # Added a caching option which saves everything related to a particular file index (key)
+        self._cache = {}
+        self.display = display
+        self._file_index = file_index
         self.algo = algo
         self.matcher = matcher
         self.threshold = threshold
@@ -32,9 +42,16 @@ class Matcher:
         :param idx: An integer file index for the images to match.
         :return: None
         """
+        self._file_index = idx
         self._img1, self._img2 = read_images(idx)
+        if self.display is VERTICAL_REPRESENTATION:
+            self._img1 = cv2.rotate(self._img1, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            self._img2 = cv2.rotate(self._img2, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    def get_matches(self)->np.ndarray:
+        # I Changed this function to keep each frame array into the cache for later use
+        self._cache[self._file_index] = {FRAMES: (self._img1, self._img2), LEFT: None, RIGHT: None}
+
+    def get_matches(self) -> np.ndarray:
         return self._matches
 
     def get_kp(self):
@@ -67,11 +84,17 @@ class Matcher:
             print(f"first descriptor in im1: \n{self._img1_dsc[0]}")
             print(f"first descriptor in im2: \n{self._img2_dsc[0]}")
 
-    def detect_and_compute(self):
+    def detect_and_compute(self, file_index=0):
         """Detect and compute the key-points and descriptors for the img1 and img2 images using the algorithm specified
         in the constructor."""
+        self.read_images(file_index)
         self._img1_kp, self._img1_dsc = self.algo.detectAndCompute(self._img1, None)
         self._img2_kp, self._img2_dsc = self.algo.detectAndCompute(self._img2, None)
+        self._cache[self._file_index][LEFT] = (self._img1_kp, self._img1_dsc)
+        print(self._cache[self._file_index][LEFT])
+        self._cache[self._file_index][RIGHT] = (self._img2_kp, self._img2_dsc)
+        print(self._cache[self._file_index][RIGHT])
+
 
     def find_matching_features(self, with_significance_test=True, debug=False):
         """Find matching features between the img1 and img2 images using the matcher specified in the constructor.
@@ -83,6 +106,7 @@ class Matcher:
             self.apply_threshold(debug)
         else:
             self._matches = self.matcher.knnMatch(self._img1_dsc, self._img2_dsc, k=1)
+        self._cache[self._file_index][MATCHES] = self._matches
 
     def apply_threshold(self, debug=False):
         """Filter matches based on a threshold value and return the filtered matches.
@@ -97,27 +121,32 @@ class Matcher:
             print(f"When applying significance test with threshold {self.threshold}, out of {len(self._matches)} matches {len(filtered)} matches remained")
         self._matches = filtered
 
-    def draw_matches(self, num_of_matches=5000, debug=False):
-        """
-        This function draws the matches found by the matching algorithm between the two input images.
-        :param num_of_matches: Maximum number of matches to be randomly chosen and displayed
-        :return: None
-        """
-        matches = self._matches
-        im1, im2 = self._img1, self._img2
-        current_num_of_matches = len(self._matches)
-        if current_num_of_matches >= num_of_matches:
-           matches = random.sample(matches, num_of_matches)
-        print(f"drawing {len(matches)} matches")
-        if debug:
-            im_1_pt = self._img1_kp[212]
-            im_2_pt = self._img2_kp[135]
-            im1 = cv2.circle(self._img1,(np.int(im_1_pt.pt[0]),np.int(im_1_pt.pt[1])),10, 3,thickness=3, lineType=8, shift=0)
-            im2 = cv2.circle(self._img2, (np.int(im_2_pt.pt[0]), np.int(im_2_pt.pt[1])), 10, 3, thickness=3, lineType=8,
-                             shift=0)
-        img3 = cv2.drawMatchesKnn(im1, self._img1_kp, im2, self._img2_kp, matches, None,
-                                  flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-        plt.figure(figsize=(15, 15))
-        plt.imshow(img3)
-        plt.show()
+    def match_between_consecutive_frames(self, prev_frame_index, cur_frame_index, debug=True):
+        """
+        given to frames indices, this function will compute their matching points and cache it.
+        :param prev_frame_index:
+        :param cur_frame_index:
+        :return:
+        """
+        prev_kps, prev_dsc = self._cache[prev_frame_index][LEFT] # Returns kp, dsc of previous left image
+        cur_kps, cur_dsc = self._cache[cur_frame_index][LEFT] # Returns kp, dsc of current left image
+
+        prev_im1 = self._cache[prev_frame_index][FRAMES][0]
+        cur_im1 = self._cache[cur_frame_index][FRAMES][0]
+        matches = self.matcher.knnMatch(cur_dsc, prev_dsc, k=2)
+
+        self._cache[cur_frame_index][CONSECUTIVE] = matches
+        matches = Matcher.apply_thresholds(matches, .9)
+
+        draw_matches(matches, prev_im1, cur_im1, prev_kps, cur_kps, num_of_matches=20, debug=False, display=self.display)
+        return matches
+
+
+    @staticmethod
+    def apply_thresholds(matches, threshold):
+        filtered = []
+        for i, (m,n) in enumerate(matches):
+            if m.distance < threshold * n.distance:
+                filtered.append([m])
+        return filtered
--- utils/utils.py
+++ utils/utils.py
@@ -17,13 +17,37 @@ def read_images(idx) -> (np.ndarray, np.ndarray):
     img_name = '{:06d}.png'.format(idx)
     _img1 = cv2.imread(DATA_PATH + f'image_0{SEP}' + img_name, 0)
     _img2 = cv2.imread(DATA_PATH + f'image_1{SEP}' + img_name, 0)
+
     return _img1, _img2
 
 
-def coords_from_kps(matches, kp1,kp2):
+def coords_from_kps(matches, kp1, kp2):
+    """
+    Extracts the (x, y) coordinates of matched features in two images and their corresponding indices in the original
+    keypoint arrays.
+
+    Parameters:
+    -----------
+    matches : list of cv2.DMatch objects
+        A list of matching feature descriptors, each containing the queryIdx and trainIdx attributes.
+    kp1 : list of cv2.KeyPoint objects
+        A list of keypoint objects representing features in the first image.
+    kp2 : list of cv2.KeyPoint objects
+        A list of keypoint objects representing features in the second image.
+
+    Returns:
+    --------
+    x1, y1, x2, y2 : ndarray
+        Four 1D arrays of shape (num_matches,) containing the x and y coordinates of matched features in the first
+        and second images, respectively.
+    indices_mapping : ndarray
+        An array of shape (2, num_matches) containing the corresponding indices of the matched features in the
+        original kp1 and kp2 arrays.
+    """
     query_idxs = np.array([match[0].queryIdx for match in matches])
     train_idxs = np.array([match[0].trainIdx for match in matches])
     indices_mapping = np.stack((query_idxs,train_idxs))
+
     x1, y1 = np.array([kp1[idx].pt for idx in query_idxs]).T
     x2, y2 = np.array([kp2[idx].pt for idx in train_idxs]).T
 
@@ -42,3 +66,53 @@ def read_cameras():
         mat1 = np.linalg.inv(k) @ m1
         mat2 = np.linalg.inv(k) @ m2
         return k, mat1, mat2
+
+
+def _rectified_stereo_classifier(y1: np.ndarray, y2:np.ndarray, thresh=1):
+    """
+    An helper function for rectificatied_stereo_pattern.
+    :param y1: The y-coordinate of all kp1.pt
+    :param y2: The y-coordinate of the corresponding y1 (matched by taking the trainIdx)
+    :param thresh: Threshold value to determine how to reject matching points
+    :return: inliers and outliers nd arrays
+    """
+    deviations = np.abs(y1 - y2)
+    inliers_idx = np.argwhere(deviations <= thresh)[:,0]
+    outliers_idx = np.argwhere(deviations > thresh)[:,0]
+    return inliers_idx, outliers_idx
+
+
+def rectificatied_stereo_pattern(y1, y2, indices_mapping, thresh=1):
+    """
+    For each pair of points, this function will return a classification of points to two groups: Inliers and Outliers.
+    This is done for each image.
+    :param y1: The y-coordinate of all kp1.pt (Left image of the KITTI dataset)
+    :param y2: The y-coordinate of the corresponding y1, matched by taking the trainIdx (Right image of the KITTI dataset).
+    :param thresh: Threshold value to determine how to reject matching points
+    :return:
+    """
+    inliers_idx, outliers_idx = _rectified_stereo_classifier(y1, y2, thresh)
+    img1in, img2in = indices_mapping[0, inliers_idx], indices_mapping[1, inliers_idx]
+    img1out, img2out = indices_mapping[0, outliers_idx], indices_mapping[1, outliers_idx]
+    return img1in, img2in, img1out, img2out
+
+def array_to_dict(arr):
+    """
+    Convert a 2D numpy array to a dictionary.
+
+    Parameters:
+    -----------
+    arr : ndarray
+        A 2D numpy array with shape (2, N).
+
+    Returns:
+    --------
+    dict
+        A dictionary where all values in the 1st row are keys, and all values in the 2nd row are values.
+    """
+    dict_obj = {}
+    for i in range(arr.shape[1]):
+        key = arr[0, i]
+        value = arr[1, i]
+        dict_obj[key] = value
+    return dict_obj
