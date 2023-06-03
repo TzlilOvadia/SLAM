30cfa40e0b8027d5e74fdd94100d10582fff6c0a
Tzlil Ovadia <tzlil.ovadia@mail.huji.ac.il>
2023-06-03 22:54:57 +0300

    Added Class and functionality for Track DB

---
+++ models/Constants.py
@@ -0,0 +1,16 @@
+
+#############################################
+################# Constants #################
+#############################################
+KPS = 0
+DSC = 2
+HORIZONTAL_REPRESENTATION = 0
+VERTICAL_REPRESENTATION = 1
+DEFAULT_POV = 0
+TOP_POV = 80
+SIDE_POV = -120
+MATCHES = 2
+LEFT = 0
+RIGHT = 1
+CONSECUTIVE = 3
+FRAMES = 4
---
+++ models/TrackDatabase.py
@@ -0,0 +1,81 @@
+# noinspection PyCompatibility
+FEATURES = 1
+EVEN = 0
+ODD = 1
+
+class TrackDatabase:
+    def __init__(self):
+        self._tracks = {}  # Dictionary to store tracks
+        self._frame_ids = {}  # Dictionary to store frame ids
+        self.__next_free_trackId = 0
+        self._last_insertions = {ODD: {}, EVEN: {}}  # Dictionary to store last left_kps indices (Keys) and a matching trackId (Values)
+
+    def add_track(self, trackId, frameId, feature_location_prev, kp_prev, kp_cur):
+        # cur_feature = (kp_cur, feature_location_cur, frameId)
+        prev_feature = (kp_prev, feature_location_prev, frameId)
+
+        try:
+            self._tracks[trackId].append(prev_feature)
+
+        except KeyError:
+            self._tracks[trackId] = [prev_feature]
+
+        finally:
+            self._frame_ids[frameId].add(trackId)
+            self._last_insertions[(frameId + 1) % 2][kp_cur] = trackId
+
+    def get_track_ids_for_frame(self, frameId):
+        """
+        returns all the TrackIds that appear on a given FrameId.
+        :param frameId:
+        :return:
+        """
+        if frameId in self._frame_ids:
+            return list(self._frame_ids[frameId])
+        else:
+            return []
+
+    def get_frame_ids_for_track(self, trackId):
+        if track_id in self._tracks:
+            return list(self._tracks[trackId].keys())
+        else:
+            return []
+
+    def get_feature_locations(self, frameId, trackId):
+        if trackId in self._tracks and frameId in self._tracks[trackId]:
+            return self._tracks[trackId][frameId]
+        else:
+            return None
+
+    def extend_database(self, frameId, matches):
+        for track_id, feature_location in matches.items():
+            self.add_track(track_id, frameId, feature_location)
+
+    def serialize(self, file_path):
+        data = {
+            'tracks': self._tracks,
+            'frame_ids': self._frame_ids
+        }
+        with open(file_path, 'wb') as f:
+            pickle.dump(data, f)
+
+    def deserialize(self, file_path):
+        with open(file_path, 'rb') as f:
+            data = pickle.load(f)
+            self._tracks = data['tracks']
+            self._frame_ids = data['frame_ids']
+
+    def generate_new_track_id(self):
+        new_trackId = self.__next_free_trackId
+        self.__next_free_trackId += 1
+        return new_trackId
+
+    def get_kp_trackId(self, kp, frameId):
+        try:
+            return self._last_insertions[frameId % 2][kp]
+        except KeyError:
+            return
+
+    def prepare_to_next_pair(self, frameId):
+        self._frame_ids[frameId] = set()
+        self._last_insertions[(frameId + 1) % 2] = {}
